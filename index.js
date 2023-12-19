6fe09c3f5dceb5165c638d0a002b39778d555ce2
ikelina69 <150611422+ikelina69@users.noreply.github.com>
2023-12-19 12:13:29 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction(){
+  let x = document.getElementById("search1");
+  let y = document.getElementById("search");
+
+  if (x.type === "string"){
+      y.display  = "String";
+    }
+
+} 
